d0d93a09aa9938f88803a495dde22229473ba13c
Joe Koberg <joe.koberg@versionone.com>
2012-08-31 13:34:19 -0400

    wip

--- bowling.erl
+++ bowling.erl
@@ -17,15 +17,44 @@ loop() ->
         
 
 score([X,Y,Z|Tail])
-    when X == 10    -> {frame, strike, 10 + Y + Z, [Y,Z|Tail]};    
+    when X == 10    -> {frame, strike, 10 + Y + Z, [Y,Z|Tail]};
+    
 score([X,Y,Z|Tail])
-    when X+Y == 10  -> {frame, spare, X + Y + Z, [Z|Tail]};    
+    when X+Y == 10  -> {frame, spare, X + Y + Z, [Z|Tail]};
+    
 score([X,Y|Tail])
     when X+Y < 10   -> {frame, normal, X + Y, Tail};
+    
 score(Tail)            -> {roll_again, none, 0, Tail}
 
 frame_scorer(Score, []) ->
     receive
-        {roll, Downed} -> frame_scorer(
+        {roll, Downed} -> ok;
+    end.
 
+alley_manager() ->
+    receive
+        {peopleComingIn, PlayerGroup} -> 
+            {ok, Lane} = allocate_lane().
+            Lane ! {prepareForPlayers, PlayerGroup};  % allocate a lane
+        {gameFinished, LaneNumber, PlayerScores} -> ok; % deallocate a lane
+    end.
+    
+lane() ->
+    receive
+        {prepareForPlayers, PlayerGroup} -> 
+           self() ! {resetPins};
+        {foulSensorTripped, Player} -> ok;
+        {ballThrown, Player, Skill} -> 
+           ballsdropped = 10 * Skill
+           recordDrop ballsdropped;
+        {resetPins} -> ok;
+    end.
+
+player() ->
+    receive
+        {goToLane, Lane} -> ok;
+        {rollAgain, Lane} -> Lane ! {ballThrown, self(), 1.0} ;
+        {gameOver, Lane, Score} -> ok;
+    end.
 
